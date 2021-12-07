258a1becfb150ddd525cf534ecd51a6dc7c9cc4a
neongm <chura2013c@gmail.com>
2021-12-07 20:06:13 +0500

    enabled whitenoise for static files on deploy

--- main/settings.py
+++ main/settings.py
@@ -33,7 +33,8 @@ ALLOWED_HOSTS = [
     '192.168.1.87',
     'localhost',
     '127.0.0.1',
-    '192.168.2.88'
+    '192.168.2.88',
+    ".herokuapp.com",
 ]
 
 
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',             # STATIC FILES ON DEPLOY
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
