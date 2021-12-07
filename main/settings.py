089fec09db4e542da4b47f2e0971e816d1fdc437
neongm <chura2013c@gmail.com>
2021-12-08 01:09:35 +0500

    turned off debug mode

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '192.168.1.87',
